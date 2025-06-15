3f261d4be1a424de9f9c291c9a228dc367afec84
savio <savio_ls@hotmail.com>
2025-06-15 12:50:46 -0300

    Adicionado o RabbitMQ com o processo de envio para exchange.

--- src/main/java/com/sensuscorp/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/sensuscorp/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -3,7 +3,10 @@ package com.sensuscorp.temperature.processing.api.controller;
 import com.sensuscorp.temperature.processing.api.model.TemperatureLogOutput;
 import com.sensuscorp.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +14,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 
+import static com.sensuscorp.temperature.processing.infrastructure.rabbitmq.RabbitMQConfig.FANOUT_EXCHANGE_NAME;
+
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/temperatures/data")
 @Slf4j
+@RequiredArgsConstructor
 public class TemperatureProcessingController {
+
+    private final RabbitTemplate rabbitTemplate;
 
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public void data(@PathVariable TSID sensorId, @RequestBody String input) {
@@ -39,6 +47,15 @@ public class TemperatureProcessingController {
 
         log.info(logOutput.toString());
 
+        String exchange = FANOUT_EXCHANGE_NAME;
+        String routingKey = "";
+        Object payload = logOutput;
+
+        MessagePostProcessor messagePostProcessor = message -> {
+            message.getMessageProperties().setHeader("sensorId", logOutput.getSensorId().toString());
+            return message;
+        };
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload, messagePostProcessor);
     }
 
 }
